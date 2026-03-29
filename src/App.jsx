cbbbacf7416d3c8fc1092513370b073ccf81f9ed
Satyam Garodia <91243194+Satyamgarodia@users.noreply.github.com>
2026-03-29 16:35:38 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,13 @@ function App() {
                 <Route path="/ai/1" element={<Navigate to="/ai/1.pdf" />} />
         <Route path="/2" element={<Navigate to="/ai/2.pdf" />} />
         <Route path="/3" element={<Navigate to="/ai/3.pdf" />} />
-        <Route path="4" element={<Navigate to="/ai/4.pdf" />} />
-        <Route path="5" element={<Navigate to="/ai/5.pdf" />} />
-        <Route path="6" element={<Navigate to="/ai/6.pdf" />} />
-        <Route path="7" element={<Navigate to="/ai/7.pdf" />} />
-        <Route path="8" element={<Navigate to="/ai/8.pdf" />} />
-        <Route path="9" element={<Navigate to="/ai/9.pdf" />} />
-        <Route path="10" element={<Navigate to="/ai/10.pdf" />} />
+        <Route path="/4" element={<Navigate to="/ai/4.pdf" />} />
+        <Route path="/5" element={<Navigate to="/ai/5.pdf" />} />
+        <Route path="/6" element={<Navigate to="/ai/6.pdf" />} />
+        <Route path="/7" element={<Navigate to="/ai/7.pdf" />} />
+        <Route path="/8" element={<Navigate to="/ai/8.pdf" />} />
+        <Route path="/9" element={<Navigate to="/ai/9.pdf" />} />
+        <Route path="/10" element={<Navigate to="/ai/10.pdf" />} />
 
       </Routes>
     </BrowserRouter>
