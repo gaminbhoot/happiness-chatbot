375cd79bba775470b4617b8a3dabbb0d06a9a0b9
Satyam Garodia <91243194+Satyamgarodia@users.noreply.github.com>
2026-03-30 14:01:19 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,6 @@ function App() {
         {/* <Route path="/" element={<GoogleGeminiEffectDemo />} /> */}
         <Route path="/" element={<Chatbot />} />
         <Route path="/chat" element={<Chatbot />} />
-        <Route path="/ai" element={<Navigate to="/ai/index.pdf" />} />
-        <Route path="/ai/1" element={<Navigate to="/ai/1.pdf" />} />
-        <Route path="/ai/2" element={<Navigate to="/ai/2.pdf" />} />
-        <Route path="/ai/3" element={<Navigate to="/ai/3.pdf" />} />
-        <Route path="/ai/4" element={<Navigate to="/ai/4.pdf" />} />
-        <Route path="/ai/5" element={<Navigate to="/ai/5.pdf" />} />
-        <Route path="/ai/6" element={<Navigate to="/ai/6.pdf" />} />
-        <Route path="/ai/7" element={<Navigate to="/ai/7.pdf" />} />
-        <Route path="/ai/8" element={<Navigate to="/ai/8.pdf" />} />
-        <Route path="/ai/9" element={<Navigate to="/ai/9.pdf" />} />
-        <Route path="/ai/10" element={<Navigate to="/ai/10.pdf" />} />
-        <Route path="/ai/1" element={<Navigate to="/ai/1.pdf" />} />
-        <Route path="/2" element={<Navigate to="/ai/2.pdf" />} />
-        <Route path="/3" element={<Navigate to="/ai/3.pdf" />} />
-        <Route path="/4" element={<Navigate to="/ai/4.pdf" />} />
-        <Route path="/5" element={<Navigate to="/ai/5.pdf" />} />
-        <Route path="/6" element={<Navigate to="/ai/6.pdf" />} />
-        <Route path="/7" element={<Navigate to="/ai/7.pdf" />} />
-        <Route path="/8" element={<Navigate to="/ai/8.pdf" />} />
-        <Route path="/9" element={<Navigate to="/ai/9.pdf" />} />
-        <Route path="/10" element={<Navigate to="/ai/10.pdf" />} />
 
       </Routes>
     </BrowserRouter>
